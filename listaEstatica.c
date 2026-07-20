72d2233bbf15856c0a30686e16634e253faae560
André Brandão <andrebrandao@MacBook-Pro-de-Andre.local>
2026-07-20 13:52:49 -0300

    Criação da estrutura lista

--- listaEstatica.c
+++ listaEstatica.c
@@ -2,3 +2,15 @@
 
 #include <stdlib.h>
 
+// Cria uma lista estática vazia
+// Entrada: nenhuma
+// Retorno: ponteiro para a lista criada
+// Pré-condição: nenhuma
+// Pós-condição: lista inicializada e vazia
+Lista* cria_lista_vazia(void) {
+    Lista* nova_lista = (Lista*) malloc(sizeof(Lista));
+    nova_lista->primeiro = 0;
+    nova_lista->ultimo = 0;
+    nova_lista->pos = -1;
+    return nova_lista;
+}
--- listaEstatica.h
+++ listaEstatica.h
@@ -3,6 +3,23 @@
 
 #include <stdio.h>
 
+#define TAM_MAX 1000    // número máximo de itens na lista
 
+typedef int TipoItem/   // representa o tipo de elementos da lista
+
+// Estrutura para lista estática
+typedef struct {
+    TipoItem item[TAM_MAX];
+    int primeiro;
+    int ultimo;
+    int pos;    // usado para implementar iterador
+} Lista;
+
+// Cria uma lista estática vazia
+// Entrada: nenhuma
+// Retorno: ponteiro para a lista criada
+// Pré-condição: nenhuma
+// Pós-condição: lista inicializada e vazia
+Lista* cria_lista_vazia(void);
 
 #endif /* listaEstatica_h */
